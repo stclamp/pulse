c9f2aa9b05353663105670a04b6ca310069551c7
Pavel <whatisclamp@gmail.com>
2021-01-08 13:35:26 +0200

    Add carousel

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1000,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/carousel/previous.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/carousel/next.png"></button>'
+      });
+  });
