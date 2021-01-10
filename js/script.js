52053b264f2ee76703bab79bbe42f8441e8c3d19
Pavel <whatisclamp@gmail.com>
2021-01-10 12:53:15 +0200

    fix slider

--- js/script.js
+++ js/script.js
@@ -2,8 +2,16 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1000,
-        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="../img/carousel/previous.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/carousel/next.png"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/carousel/next.png"></button>',
+        responsive: [
+            {
+                    breakpoint: 992,
+                    settings: {
+                        dots: true,
+                        arrows: false
+                }
+            }
+        ]
       });
   });
