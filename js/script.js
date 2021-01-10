c25bd4b911477a42edc69e02fa2e77c01f0c3cc6
Pavel <whatisclamp@gmail.com>
2021-01-10 20:21:36 +0200

    Fix slider 2.0.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    navPosition: 'bottom',
+    speed: 1000,
+    responsive: {
+        1: {
+            nav: true   
+        },
+        992: {
+            nav: false
+        }
+    }
+  });
 
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/carousel/previous.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/carousel/next.png"></button>',
-        responsive: [
-            {
-                    breakpoint: 992,
-                    settings: {
-                        dots: true,
-                        arrows: false
-                }
-            }
-        ]
-      });
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
   });
