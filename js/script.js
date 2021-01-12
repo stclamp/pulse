6f1e6a8bf7da81cf498cd9751bf080bf704041a4
Pavel <whatisclamp@gmail.com>
2021-01-12 11:28:46 +0200

    Tabs and maps

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const slider = tns({
         1: {
             nav: true   
         },
-        992: {
+        1200: {
             nav: false
         }
     }
@@ -25,3 +25,28 @@ document.querySelector('.prev').addEventListener('click', function () {
   document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
   });
+
+
+  (function($) {
+    $(function() {
+      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.tabs').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+      function toggleSlide(item) {
+        $(item).each(function(i){
+          $(this).on('click', function(e){
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          });
+        });
+      }
+
+      toggleSlide('.catalog-item__link');
+      toggleSlide('.catalog-item__back');
+    });
+  })(jQuery);
